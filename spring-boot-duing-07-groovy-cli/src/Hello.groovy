185d67df4b228ac1f0805913dbf87abae5a3509d
XiaoWangTongXue0128 <1440057838@qq.com>
2023-10-25 22:10:27 +0800

    springboot-CLI-groovy

---
+++ spring-boot-duing-07-groovy-cli/src/Hello.groovy
@@ -0,0 +1,7 @@
+//可以直接输出打印而不需要写类
+//def类型可以不指定数据类型，类似jsp中的var
+def value="hello world"
+value=123
+println(value)
+println(value.class)
+
---
+++ spring-boot-duing-07-groovy-cli/src/HelloCLI.groovy
@@ -0,0 +1,11 @@
+
+@Grab("org.springframework.boot:spring-boot-starter-parent:2.2.0.RELEASE")
+//@Controller
+class HelloCLI {
+//    用不了
+//    @RequestMapping("/")
+//    String home(Model model) {
+//        model.addAttribute("name", "Hello World!")
+//        return "welcome"
+//    }
+}
